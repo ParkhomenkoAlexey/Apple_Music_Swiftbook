9d4811d93ab5c96cd445b77b0ee0a5865baf4d41
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2019-07-17 16:23:31 +0500

    Initial Commit

---
+++ Apple Music Swiftbook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Apple Music Swiftbook
+//
+//  Created by Алексей Пархоменко on 17/07/2019.
+//  Copyright © 2019 Алексей Пархоменко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
