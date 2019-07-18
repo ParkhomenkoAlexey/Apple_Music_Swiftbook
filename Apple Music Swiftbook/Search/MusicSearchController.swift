56182b88295b2e41082f5fbe40d428c9076c8eda
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2019-07-18 23:48:06 +0500

    Урок 2: Setup UITableViewController & SearchController

--- Apple Music Swiftbook/Search/MusicSearchController.swift
+++ Apple Music Swiftbook/Search/MusicSearchController.swift
@@ -10,9 +10,50 @@ import UIKit
 
 class MusicSearchController: UITableViewController {
     
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    let tracks = [Track(trackName: "bad guy", artistName: "Billie Eilish"),
+                  Track(trackName: "bury a friend", artistName: "Billie Eilish")
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupSearchBar()
+        setupTableView()
+    }
+    
+    fileprivate func setupSearchBar() {
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.searchBar.delegate = self
+    }
+    
+    fileprivate func setupTableView() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+    }
+    
+    // MARK: - UITableViewDelegate, UITableViewDataSource
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tracks.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        let track = tracks[indexPath.row]
+        cell.textLabel?.text = "\(track.trackName)\n\(track.artistName)"
+        cell.textLabel?.numberOfLines = 2
+        cell.imageView?.image = #imageLiteral(resourceName: "appicon")
+        return cell
+    }
+}
+
+// MARK: - UISearchBarDelegate
+
+extension MusicSearchController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
     }
 }
 
---
+++ Apple Music Swiftbook/SearchResults.swift
@@ -0,0 +1,16 @@
+//
+//  PodcastResponse.swift
+//  PodcastsApp
+//
+//  Created by Алексей Пархоменко on 07/07/2019.
+//  Copyright © 2019 Алексей Пархоменко. All rights reserved.
+//
+
+import Foundation
+
+struct Track: Decodable {
+    let trackName: String
+    let artistName: String
+}
+
+
